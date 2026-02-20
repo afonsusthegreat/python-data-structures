42da361a0f0b05114a8237012ab1254a4bfc0389
Pedro <pedrohmedeiros909@gmail.com>
2026-02-20 18:50:29 -0300

    feat: implement basic binary search

---
+++ leetcode/binary_search/busca.py
@@ -0,0 +1,13 @@
+def busca(nums, alvo):
+    esq = 0
+    dir = len(nums) + 1
+    while esq <= dir:
+        meio = (esq + dir) // 2
+
+        if nums[meio] == alvo:
+            return meio
+        elif nums[meio] < alvo:
+            esq = meio + 1
+        elif nums[meio] > alvo:
+            dir = meio - 1
+    return -1
